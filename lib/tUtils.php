463ee77d3a57a40ade00187219eac99219836572
tougaj <sashamolodec@gmail.com>
2023-09-15 07:57:39 +0300

    Додане перекодування дати з нідерландського Деякі сервери там

--- lib/tUtils.php
+++ lib/tUtils.php
@@ -4,19 +4,27 @@ function translate_date_to_english($string) {
 	$string = mb_strtolower($string); // переводим строку в нижний регистр
 	$string = str_replace(array('года', 'год', 'року', 'рік'), '', $string); // удаляем слова "года" и "год"
 
-	// заменяем русские названия месяцев на английские
+	// змінюємо російські назви місяців на англійські
 	$string = str_replace(
 		array('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'),
 		array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'),
 		$string
 	);
 
+	// змінюємо українські назви місяців на англійські
 	$string = str_replace(
 		array('січня', 'лютого', 'березня', 'квітня', 'травня', 'червня', 'липня', 'серпня', 'вересня', 'жовтня', 'листопада', 'грудня'),
 		array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'),
 		$string
 	);
 
+	// змінюємо нідерландські назви місяців на англійські
+	$string = str_replace(
+		array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'),
+		array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'),
+		$string
+	);
+	
 	return $string;
 
 	// $timestamp = strtotime($string);
